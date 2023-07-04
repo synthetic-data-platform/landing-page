90dde7ddb654fee3508e7767e60e0fb7ccaaac2e
RAHUl-KAD <kadamrahulj0909@gmail.com>
2023-07-05 03:07:31 +0530

    updated ui for mobile view

--- components/Header.tsx
+++ components/Header.tsx
@@ -67,7 +67,7 @@ export default function Header() {
           <nav
             className={`flex-col items-center flex-grow ${
               open ? "flex" : "hidden"
-            } md:pb-0 md:flex md:justify-end md:flex-row`}
+            } md:pb-0 md:flex md:justify-end md:flex-row mt-10 sm:mt-0`}
           >
             <a
               className="px-2 lg:px-6 py-2 md:px-3 text-sm font-medium text-black hover:text-accent-400 lg:ml-auto"
