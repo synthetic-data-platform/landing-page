b32d4aa774110b008d512aee3ffab6ff72740f98
RAHUl-KAD <kadamrahulj0909@gmail.com>
2023-10-28 03:25:56 +0530

    ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -66,7 +66,7 @@ const Home: NextPage = () => {
                 <div
                     className="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
                 </div>
-                <a href="/playground" title="Get quote now"
+                <a href="/playground"
                     className="relative inline-flex items-center justify-center px-6 py-3 text-base font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
                     role="button">Try it for yourself
                 </a>
